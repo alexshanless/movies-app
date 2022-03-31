a19302a99847cfeac194977977c2b99976b73f75
alexshanless <alex.shanless@gmail.com>
2022-03-30 18:07:37 -0700

    https

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const API_URL =
-  "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dfd4c36a04b956a2d48d9b1607cbd58f&page=1";
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dfd4c36a04b956a2d48d9b1607cbd58f&page=1';
 
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'http://api.themoviedb.org/3/search/movie?api_key=dfd4c36a04b956a2d48d9b1607cbd58f&query="';
 
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const main = document.getElementById("main");
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getMovies(API_URL);
 
 function showMovies(movies) {
-  main.innerHTML = "";
-  movies.forEach((movie) => {
+  main.innerHTML = '';
+  movies.forEach(movie => {
     const { title, poster_path, vote_average, overview } = movie;
 
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
     movieEl.innerHTML = `
     
 <img src="${IMG_PATH + poster_path}" alt="${title}"
@@ -37,11 +37,11 @@ function showMovies(movies) {
 
 function getClassByRate(vote) {
   if (vote >= 8) {
-    return "green";
+    return 'green';
   } else if (vote >= 5) {
-    return "orange";
+    return 'orange';
   } else {
-    return "red";
+    return 'red';
   }
 }
 
@@ -51,15 +51,15 @@ async function getMovies(url) {
   showMovies(data.results);
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 
   const searchTerm = search.value;
   console.log(search.value);
 
-  if (searchTerm && searchTerm !== "") {
+  if (searchTerm && searchTerm !== '') {
     getMovies(SEARCH_API + searchTerm);
-    search.value = "";
+    search.value = '';
   } else {
     window.location.reload();
   }
